dc1540fa5f4c51ca032c331520a7eb283b6928d6
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-03-22 12:17:50 +0000

    lint formatting

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const keys = array.keys();
-  for (let idx of keys) {
-    array[idx] = appendString + array[idx];
-  }
+	for (let idx of array) {
+		let value = array[idx];
+		array[idx] = appendString + value;
+	};
 
-  return array;
+	return array;
 }
