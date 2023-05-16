fe5a7bc3c1b1021307c6f89875520b9e3f4bdb32
Olamide Joseph Williams <circleofwilliams@gmail.com>
2023-05-16 21:20:24 +0000

    Node js Basic and express

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Display messages on the standard output.
+ * @param {String} msg message to be displayed.
+ * @author Olamide Williams <https://github.com/circleofwilliams>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,3 @@
+# Node JS Basic
+
+This folder contaisn files and sub-folders that answers project task aimed to teach the basics of node js as a backend server.
